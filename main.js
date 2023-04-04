ffb8d84c542e993df4993cfeb07d54cdf77351cf
Filipe Andrade <filipe.law@gmail.com>
2023-04-03 23:22:10 -0300

    pontuação

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-let palavras = ['gato', 'cachorro', 'andorinha', 'pombo', 'capivara', 'pantera', 'hipopotamo', 'girafa',
- 'jacare', 'rinoceronte', 'vaca', 'ornitorrinco', 'abelha', 'alpaca', 'avestruz', 'baleia', 'bode', 
- 'camelo', 'castor', 'cavalo', 'coala', 'cobra', 'coelho', 'crocodilo', 'elefante', 'galinha', 
- 'golfinho', 'gorila', 'guaxinim', 'iguana', 'jabuti', 'lagosta', 'leopardo', 'lobo', 'lontra', 
- 'macaco', 'morcego', 'ovelha', 'panda', 'papagaio', 'pardal', 'pato', 'pinguim', 'porco', 'rato', 
- 'sapo', 'tamandua', 'tartaruga', 'tatu', 'tigre', 'touro', 'tubarao', 'tucano', 'urso', 'urubu', 'zebra']
+let palavras = ['gato', 'cachorro', 'andorinha', 'pombo', 'capivara', 'pantera', 'hipopotamo', 'girafa','jacare', 'rinoceronte', 'vaca', 'ornitorrinco', 'abelha', 'alpaca', 'avestruz', 'baleia', 'bode', 'camelo', 'castor', 'cavalo', 'coala', 'cobra', 'coelho', 'crocodilo', 'elefante', 'galinha', 'golfinho', 'gorila', 'guaxinim', 'iguana', 'jabuti', 'lagosta', 'leopardo', 'lobo', 'lontra', 'macaco', 'morcego', 'ovelha', 'panda', 'papagaio', 'pardal', 'pato', 'pinguim', 'porco', 'rato', 'sapo', 'tamandua', 'tartaruga', 'tatu', 'tigre', 'touro', 'tubarao', 'tucano', 'urso', 'urubu', 'zebra']
 
 let palavra = palavras[Math.floor(Math.random() * palavras.length)]
 
-let letrasAcertadas = [], letrasErradas = []
+let letrasAcertadas = [], letrasErradas = [], pontos
+
+if(localStorage.getItem('Pontos') === null){
+    localStorage.setItem('Pontos', 0)
+    pontos = parseInt(localStorage.getItem('Pontos'))
+}else{
+    pontos = parseInt(localStorage.getItem('Pontos'))
+}
 
 atualizaTela()
 
+document.addEventListener('keydown', (e) =>{ if(e.key === 'Enter') inicia() })
 
-document.addEventListener('keydown', (e)=>{
-    let letra = e.key
+function inicia(){
+
+    let letra = document.getElementById('input').value
+
     if(eUmaLetra(letra)){
         if(letrasErradas.includes(letra) || letrasAcertadas.includes(letra)){
             alert("letra já repetida")
@@ -21,15 +26,19 @@ document.addEventListener('keydown', (e)=>{
             palavra.includes(letra) ? letrasAcertadas.push(letra) : letrasErradas.push(letra)
         }
     }
-    atualizaTela()
-})
 
-function eUmaLetra(letra){
+    atualizaTela()
+
+}
+
+function eUmaLetra(input){
     const regex = /[a-zA-Z]/
-    return regex.test(letra)
+    return regex.test(input)
 }
 
 function atualizaTela(){
+    document.getElementById('pontos').innerText = localStorage.getItem("Pontos")
+    document.getElementById('input').value = ''
     mostrarLetrasErradas()
     mostrarLetrasCertas()
     checaJogo()
@@ -60,14 +69,25 @@ function checaJogo(){
     if(palavra === divCerta.innerText){
         modal.style.display = 'flex'
         mensagem.innerText = `Você Ganhou! A palavra era: ${palavra}!`
+        salvaPontos(pontos += 1)
     }
 
     if(letrasErradas.length === 6){
         modal.style.display = 'flex'
         mensagem.innerText = `Você perdeu! A palavra era ${palavra}!`
+        if(pontos > 0) salvaPontos(pontos -= 1)
     }
 }
 
 function reiniciaJogo(){
     window.location.reload()
+}
+
+function salvaPontos(pontos){
+    parseInt(localStorage.setItem('Pontos', pontos))
+}
+
+function zerar(){
+    localStorage.setItem('Pontos', 0)
+    atualizaTela()
 }
